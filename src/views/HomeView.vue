4073d05d6eb15bc1d4db5e4ee2fe99d055de66f3
Sonia Parker <sonia.parker@ping-works.com.au>
2024-06-25 17:09:28 +1000

    add initial setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="home">
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
+  </div>
+</template>
+
+<!-- Options API script -->
+<!-- <script>
+export default {
+  data() {
+    return {
+      counter: 10
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++
+    },
+    decreaseCounter() {
+      this.counter--
+    }
+  }
+}
+</script> -->
+
+<!-- Composition API script - new -- script version -->
+<!-- <script>
+
+import {ref} from 'vue';
+
+export default {
+  setup() {
+const counter = ref(20);
+
+const increaseCounter = () => {
+  counter.value++
+}
+const decreaseCounter = () => {
+  counter.value--
+}
+return {
+  counter,
+  increaseCounter,
+  decreaseCounter
+}
+  }
+}
+</script> -->
+
+
+<!-- new script setup version -->
+<script setup>
+
+import {ref} from 'vue';
+
+const counter = ref(20);
+
+const increaseCounter = () => {
+  counter.value++
+}
+const decreaseCounter = () => {
+  counter.value--
+}
+</script>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
