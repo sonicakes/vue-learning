c465a688fd60510b56596a764dbd21e8e7100564
Sonia Parker <sonia.parker@ping-works.com.au>
2024-06-26 15:58:52 +1000

    methods, computed & watch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="home">
+    <h2> {{ appTitle }}</h2>
+  <h3> {{ counterData.title }}:</h3>
     <div>
       <button @click="decreaseCounter" class="btn">-</button>
-      <span class="counter">{{ counter }}</span>
-      <button @click="increaseCounter" class="btn">+</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter(1, $event)" class="btn">+</button>
+      <p>This number is <em>{{ oddOrEven }}</em></p>
+    </div>
+    <div class="edit">
+      <h4>Edit counter title</h4>
+      <input v-model="counterData.title" type="text">
     </div>
   </div>
 </template>
@@ -55,16 +62,38 @@ return {
 <!-- new script setup version -->
 <script setup>
 
-import {ref} from 'vue';
+import {reactive, computed, watch} from 'vue';
 
-const counter = ref(20);
+// const counter = ref(20);
+// const counterTitle = ref('My Counter');
 
-const increaseCounter = () => {
-  counter.value++
+const appTitle = 'My amazing app';
+//methods
+const increaseCounter = (amount, ev) => {
+  console.log('event', ev)
+  counterData.count +=amount;
 }
 const decreaseCounter = () => {
-  counter.value--
+  counterData.count--
 }
+
+const counterData = reactive({
+  count: 20,
+  title: 'My Counter'
+});
+//computed
+const oddOrEven = computed (() => {
+ return counterData.count % 2 === 0 ? 'even' : 'odd'
+})
+
+//watch
+watch(() => counterData.count, (newCount, oldCount) => {
+console.log('old count', oldCount);
+console.log('new count', newCount)
+if (newCount> 25) {
+  alert('we have gone past 25!')
+}
+})
 </script>
 
 <style>
